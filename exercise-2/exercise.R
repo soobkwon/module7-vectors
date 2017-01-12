e9572263bd0a595f4dbd40c0f824a1d947f937b6
Soobin Kwon <soobkwon@uw.edu>
2017-01-12 15:20:27 -0800

    Add complete Module 7 Exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -21,5 +21,8 @@ large <- z[z >= 5]
 ### Bonus ###
 
 # Replace the values in `z` that are larger than 5 with the number 5
+z[z > 5] <- 5
 
 # Replace every other value in `z` with the number 0
+z[z != 5] <- 0
+
